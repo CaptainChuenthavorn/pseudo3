0e3f55311752c1d9423c8ec7eacd98358ea6b3dd
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-04 20:59:24 +0700

    finish all

---
+++ pseudo3/2.cpp
@@ -0,0 +1,14 @@
+/*#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+int main() {
+	int x, sum, i;
+	sum = 0;
+	scanf("%d", &x);
+	for (i = 1;i <= x/2;i++) {
+		if (x% i == 0) {
+			sum += i;
+		}
+	}
+	printf("sum : %d", sum);
+	return 0;
+}*/
